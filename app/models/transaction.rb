f11495261f2f375eb6a78e079d9fd797e62fccea
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-01 16:10:04 -0600

    Remove validation for credit card expiration date in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,6 @@
 class Transaction < ApplicationRecord
   validates_presence_of :invoice_id,
                         :credit_card_number,
-                        :credit_card_expiration_date,
                         :result,
                         :created_at,
                         :updated_at
