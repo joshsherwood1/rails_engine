10ba9f4c6b317fc59aae4e0629b7e169965b3291
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-09-30 22:25:17 -0600

    Add spec tests for model, factories, and requests for item

---
+++ spec/factories/items.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :item do
+    name { "Item Qui Esse" }
+    description { "Nihil autem sit odio inventore deleniti. Est laudantium ratione distinctio laborum. Minus voluptatem nesciunt assumenda dicta voluptatum porro." }
+    unit_price { "75107" }
+    merchant_id { "1" }
+    created_at { "2012-03-27 14:53:59 UTC" }
+    updated_at { "2012-03-27 14:53:59 UTC" }
+  end
+end
