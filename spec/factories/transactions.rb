8d95237d13a3e02819946036f61162166a0bc94c
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-01 16:08:55 -0600

    Change factory test for transaction

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :transaction do
     invoice_id { 1 }
-    credit_card_number { 4654405418249632 }
+    credit_card_number { "4654405418249632" }
     #credit_card_expiration_date { nil }
     result { "success" }
     created_at { "2012-03-27 14:54:09 UTC" }
