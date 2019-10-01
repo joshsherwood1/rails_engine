de4b39fc00f271d9339a8a2a83ef04fb61aa72d4
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-01 14:44:53 -0600

    Add relationships to invoice and invoice items model spec and model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,7 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :status,
+                        :customer_id,
+                        :merchant_id,
+                        :created_at,
+                        :updated_at
 end
