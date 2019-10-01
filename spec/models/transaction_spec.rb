c8ed030ed2e3adaddc4b104f9e1adddca24c900c
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-01 16:16:03 -0600

    Update transaction spec to remove validation for credit card expiration

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Transaction, type: :model do
   describe "validations" do
     it { should validate_presence_of :invoice_id }
     it { should validate_presence_of :credit_card_number }
-    it { should validate_presence_of :credit_card_expiration_date }
     it { should validate_presence_of :result }
     it { should validate_presence_of :created_at }
     it { should validate_presence_of :updated_at }
