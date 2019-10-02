f9c7ff34e89cee5b5d978c9cd89ecdc099811ba9
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-02 15:16:28 -0600

    Add relationship betweel merchants and invoice items in merchant model spec test and merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,11 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :items
   validates_presence_of :name,
                         :created_at,
                         :updated_at
 
   def self.merchants_with_most_revenue(quantity)
-    joins(:invoices).joins(:invoice_items).group(:id).select("merchants.*, sum()").limit(quantity)
+    joins(:invoice_items).group(:id).select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue").order("revenue desc").limit(quantity)
   end
 end
