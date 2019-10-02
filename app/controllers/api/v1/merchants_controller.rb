ec58452457d2b7cd2dc211c8310c2fd6a8ad8e0e
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-02 15:23:25 -0600

    Remove action in merchants controller that belongs in nested merchants most revenue controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,9 +3,7 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.new(Merchant.all)
   end
 
-  def most_revenue_for_merchants
-    @quantity = params["quantity"].to_i
-    set_of_merchants = Merchant.merchants_with_most_revenue(@quantity)
-    render json: MerchantSerializer.new(set_of_merchants)
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 end
