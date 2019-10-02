e1a8bae970f7220304c6599f61a8f8681557b4fd
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-02 15:37:08 -0600

    Rearrange placement of routes so that namespaced route is the first route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get "/most_revenue", to: "most_revenue#index"
+      end
       resources :items, only: [:index]
-      resources :merchants, only: [:index]
+      resources :merchants, only: [:index, :show]
       resources :customers, only: [:index]
       resources :invoices, only: [:index]
       resources :invoice_items, only: [:index]
       resources :transactions, only: [:index]
-      #get "/merchants/:merchant_id/items", to: "items#index"
-      #get "/merchants/most_revenue", to: "merchants#most_revenue_for_merchants"
-      namespace :merchants do
-        get "/most_revenue", to: "most_revenue#index"
-      end
     end
   end
 end
