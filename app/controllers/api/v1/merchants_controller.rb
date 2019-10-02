f9c15bf0ba7fbe317caf23dd8651e4ebaf52d7df
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-01 22:31:08 -0600

    Start work on creating class method for merchant and accessing get request parameters in merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def most_revenue_for_merchants
-    render json: MerchantSerializer.new(Merchant.all)
+    @quantity = params["quantity"].to_i
+    set_of_merchants = Merchant.merchants_with_most_revenue(@quantity)
+    render json: MerchantSerializer.new(set_of_merchants)
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,8 @@ class Merchant < ApplicationRecord
   validates_presence_of :name,
                         :created_at,
                         :updated_at
+
+  def self.merchants_with_most_revenue(quantity)
+    joins(:invoices).joins(:invoice_items).group(:id).select("merchants.*, sum()").limit(quantity)
+  end
 end
