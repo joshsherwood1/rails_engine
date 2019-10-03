ed488ddbdeadb122feb14f4860e479e5122a199c
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-02 21:05:25 -0600

    Create test for showing one invoice in invoice request spec

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -14,4 +14,17 @@ describe "Invoices API" do
 
     expect(invoices["data"].count).to eq(3)
   end
+
+  it "can get one invoice by its id" do
+    merchant_1 = create(:merchant)
+    customer_1 = create(:customer)
+    id = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_1.id).id
+
+    get "/api/v1/invoices/#{id}"
+
+    invoice = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice["data"]["id"]).to eq(id.to_s)
+  end
 end
