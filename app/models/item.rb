5d8a8373108b7ee225c7716fa8b8b2a5b0027eb7
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-02 19:33:54 -0600

    Change order of associations in item and invoice models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  has_many :invoices, through: :invoice_items
   has_many :invoice_items
+  has_many :invoices, through: :invoice_items
   belongs_to :merchant
   validates_presence_of :name,
                         :description,
