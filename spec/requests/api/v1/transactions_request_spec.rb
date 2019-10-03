4f79d5bf63da4abc2d8b4a6e7091bac10f455aaf
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-02 21:10:36 -0600

    Add test to transaction request spec for showing one transaction

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -15,4 +15,18 @@ describe "Transactions API" do
 
     expect(transactions["data"].count).to eq(3)
   end
+
+  it "can get one transaction by its id" do
+    merchant_1 = create(:merchant)
+    customer_1 = create(:customer)
+    invoice_1 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_1.id)
+    id = create(:transaction, invoice_id: invoice_1.id).id
+
+    get "/api/v1/transactions/#{id}"
+
+    invoice = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice["data"]["id"]).to eq(id.to_s)
+  end
 end
