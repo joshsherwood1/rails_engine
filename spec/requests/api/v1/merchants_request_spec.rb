3f712c812e6d709a3096d6f6a49663babb7c30ba
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 10:20:42 -0600

    Add test to merchants request spec that tests that we can find a single merchant record based on name

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -72,4 +72,46 @@ describe "Merchants API" do
     expect(merchants["data"][1]["id"]).to eq(merchant_4.id.to_s)
     expect(merchants["data"][2]["id"]).to eq(merchant_3.id.to_s)
   end
+
+  it "finds single merchant record based on name" do
+    customer_1 = create(:customer)
+    merchant_1 = create(:merchant, name: "Schroeder-Jerde")
+    merchant_2 = create(:merchant, name: "Klein, Rempel and Jones")
+    merchant_3 = create(:merchant, name: "Willms and Sons")
+    merchant_4 = create(:merchant, name: "Cummings-Thiel")
+    merchant_5 = create(:merchant, name: "Williamson Group")
+    merchant_6 = create(:merchant, name: "Williamson Group")
+    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id)
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id)
+    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id)
+    invoice_4 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id)
+    invoice_5 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id)
+    invoice_6 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_5.id)
+    item_1 = create(:item, unit_price: 12345, merchant_id: merchant_5.id)
+    item_2 = create(:item, unit_price: 22345, merchant_id: merchant_1.id)
+    item_3 = create(:item, unit_price: 32345, merchant_id: merchant_2.id)
+    item_4 = create(:item, unit_price: 42345, merchant_id: merchant_3.id)
+    item_5 = create(:item, unit_price: 52345, merchant_id: merchant_4.id)
+    item_6 = create(:item, unit_price: 62345, merchant_id: merchant_4.id)
+    create(:invoice_item, item_id: item_1.id, invoice_id: invoice_6.id, unit_price: item_1.unit_price, quantity: 1)
+    create(:invoice_item, item_id: item_2.id, invoice_id: invoice_1.id, unit_price: item_2.unit_price, quantity: 1)
+    create(:invoice_item, item_id: item_3.id, invoice_id: invoice_2.id, unit_price: item_3.unit_price, quantity: 15)
+    create(:invoice_item, item_id: item_4.id, invoice_id: invoice_3.id, unit_price: item_4.unit_price, quantity: 1)
+    create(:invoice_item, item_id: item_5.id, invoice_id: invoice_4.id, unit_price: item_5.unit_price, quantity: 1)
+    create(:invoice_item, item_id: item_6.id, invoice_id: invoice_5.id, unit_price: item_6.unit_price, quantity: 1)
+    create(:transaction, invoice_id: invoice_1.id, result: "success")
+    create(:transaction, invoice_id: invoice_2.id, result: "success")
+    create(:transaction, invoice_id: invoice_3.id, result: "success")
+    create(:transaction, invoice_id: invoice_4.id, result: "success")
+    create(:transaction, invoice_id: invoice_5.id, result: "success")
+    create(:transaction, invoice_id: invoice_6.id, result: "success")
+
+    get "/api/v1/merchants/find?name=Cummings-Thiel"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["data"]["id"]).to eq(merchant_4.id.to_s)
+  end
 end
