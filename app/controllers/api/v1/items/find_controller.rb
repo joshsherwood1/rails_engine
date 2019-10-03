f930d4b0aa82e6acb40fd1f04253c11d2d8a7dfb
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 11:58:08 -0600

    Add conditionals to find controller nested under items that redirects requests based on if unit price, merchant id, or description is included in params

--- app/controllers/api/v1/items/find_controller.rb
+++ app/controllers/api/v1/items/find_controller.rb
@@ -10,6 +10,15 @@ class Api::V1::Items::FindController < ApplicationController
     elsif params.keys.include?("id")
       id = params["id"]
       render json: ItemSerializer.new(Item.find(id))
+    elsif params.keys.include?("description")
+      description = params["description"]
+      render json: ItemSerializer.new(Item.find_by(description: description))
+    elsif params.keys.include?("unit_price")
+      unit_price = params["unit_price"]
+      render json: ItemSerializer.new(Item.find_by(unit_price: unit_price))
+    elsif params.keys.include?("merchant_id")
+      merchant_id = params["merchant_id"]
+      render json: ItemSerializer.new(Item.find_by(merchant_id: merchant_id))
     else params.keys.include?("created_at")
       created_at = params["created_at"]
       render json: ItemSerializer.new(Item.find_by(created_at: created_at))
