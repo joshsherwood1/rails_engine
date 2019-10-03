c6e16332d45fc680034b35989e429a124f6e1e23
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 16:41:03 -0600

    Refactor show action so that there are less lines of code and add conditional to index method to redirect requests that have id and name in params in merchants find controller

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -2,17 +2,23 @@ class Api::V1::Merchants::FindController < ApplicationController
   def show
     #binding.pry
     if params.keys.include?("name")
-      name = params["name"]
-      render json: MerchantSerializer.new(Merchant.find_by(name: name))
+      render json: MerchantSerializer.new(Merchant.find_by(name: params["name"]))
     elsif params.keys.include?("updated_at")
-      updated_at = params["updated_at"]
-      render json: MerchantSerializer.new(Merchant.find_by(updated_at: updated_at))
+      merchant = Merchant.find_by(updated_at: params["updated_at"])
+      render json: MerchantSerializer.new(merchant)
     elsif params.keys.include?("id")
-      id = params["id"]
-      render json: MerchantSerializer.new(Merchant.find(id))
+      render json: MerchantSerializer.new(Merchant.find(params["id"]))
     else params.keys.include?("created_at")
-      created_at = params["created_at"]
-      render json: MerchantSerializer.new(Merchant.find_by(created_at: created_at))
+      merchant = Merchant.find_by(created_at: params["created_at"])
+      render json: MerchantSerializer.new(merchant)
+    end
+  end
+
+  def index
+    if params.keys.include?("name")
+      render json: MerchantSerializer.new(Merchant.find_by_name(params["name"]))
+    else params.keys.include?("id")
+      render json: MerchantSerializer.new(Merchant.find_by_id(params["id"]))
     end
   end
 end
