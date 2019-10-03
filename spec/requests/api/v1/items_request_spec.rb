4f2d081e634e6eaf672942f6604e84742bf4f519
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 11:31:00 -0600

    Add tests in items request spec for finding single item record based on id and name

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -25,4 +25,40 @@ describe "Items API" do
     expect(response).to be_successful
     expect(item["data"]["id"]).to eq(id.to_s)
   end
+
+  it "finds single item record based on name" do
+    merchant_1 = create(:merchant, name: "Schroeder-Jerde")
+    item_1 = create(:item, name: "Item Qui Esse", merchant_id: merchant_1.id)
+    item_2 = create(:item, name: "Item Qui Essei", merchant_id: merchant_1.id)
+    item_3 = create(:item, name: "Item Qui Esseie", merchant_id: merchant_1.id)
+    item_4 = create(:item, name: "Item Qui Esseia", merchant_id: merchant_1.id)
+    item_5 = create(:item, name: "Item Qui Esseii", merchant_id: merchant_1.id)
+    item_6 = create(:item, name: "Item Qui Esseiu", merchant_id: merchant_1.id)
+
+    get "/api/v1/items/find?name=Item Qui Esseii"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body)
+
+    expect(item["data"]["id"]).to eq(item_5.id.to_s)
+  end
+
+  it "finds single item record based on id" do
+    merchant_1 = create(:merchant, name: "Schroeder-Jerde")
+    item_1 = create(:item, name: "Item Qui Esse", merchant_id: merchant_1.id, id: 1)
+    item_2 = create(:item, name: "Item Qui Essei", merchant_id: merchant_1.id, id: 2)
+    item_3 = create(:item, name: "Item Qui Esseie", merchant_id: merchant_1.id, id: 3)
+    item_4 = create(:item, name: "Item Qui Esseia", merchant_id: merchant_1.id, id: 4)
+    item_5 = create(:item, name: "Item Qui Esseii", merchant_id: merchant_1.id, id: 5)
+    item_6 = create(:item, name: "Item Qui Esseiu", merchant_id: merchant_1.id, id: 6)
+
+    get "/api/v1/items/find?id=6"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body)
+
+    expect(item["data"]["id"]).to eq(item_6.id.to_s)
+  end
 end
