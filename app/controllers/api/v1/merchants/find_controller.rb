0d417a92f92e144a82fb9202002616f1e84e5288
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 10:43:42 -0600

    Add another conditional under show action in nested find controller under merchants that redirects requests with created_at included in params

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -4,9 +4,12 @@ class Api::V1::Merchants::FindController < ApplicationController
     if params.keys.include?("name")
       name = params["name"]
       render json: MerchantSerializer.new(Merchant.find_by(name: name))
-    else params.keys.include?("updated_at")
+    elsif params.keys.include?("updated_at")
       updated_at = params["updated_at"]
       render json: MerchantSerializer.new(Merchant.find_by(updated_at: updated_at))
+    else params.keys.include?("created_at")
+      created_at = params["created_at"]
+      render json: MerchantSerializer.new(Merchant.find_by(created_at: created_at))
     end
   end
 end
