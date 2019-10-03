d85d8997d05f131edaf8f302e3cf17d0cfe7638e
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 10:34:26 -0600

    Add conditional to show action in nested find controller under merchants that redirects requests that have an updated_at variable in the params

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -4,7 +4,9 @@ class Api::V1::Merchants::FindController < ApplicationController
     if params.keys.include?("name")
       name = params["name"]
       render json: MerchantSerializer.new(Merchant.find_by(name: name))
-    else params.keys.include?("id")
+    else params.keys.include?("updated_at")
+      updated_at = params["updated_at"]
+      render json: MerchantSerializer.new(Merchant.find_by(updated_at: updated_at))
     end
   end
 end
