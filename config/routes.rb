0c0deaa412d8173703f85f9a56b2253d38c8cd16
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-02 21:17:17 -0600

    Add route and controller action for invoice item show

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index]
       resources :invoices, only: [:index, :show]
-      resources :invoice_items, only: [:index]
+      resources :invoice_items, only: [:index, :show]
       resources :transactions, only: [:index, :show]
     end
   end
