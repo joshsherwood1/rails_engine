083f1010ce4310a48e005952f9ed9ac8ea87f4f4
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-02 21:21:29 -0600

    Add test to customers request spec that tests that a single customer can be shown

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Customers API" do
   it "sends a list of customers" do
-    
+
     create_list(:customer, 3)
 
     get '/api/v1/customers'
@@ -13,5 +13,16 @@ describe "Customers API" do
 
     expect(customers["data"].count).to eq(3)
 
+  end
+
+  it "can get one customer by its id" do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer["data"]["id"]).to eq(id.to_s)
   end
 end
