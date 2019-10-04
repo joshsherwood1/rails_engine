57bfbebfd1986956f7e3055e51da1e9fe93298a2
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-04 00:15:14 -0600

    Add index action to transactions find controller with conditionals that route requests based on if credit card number, credit card expiration date, result, invoice id, id, created at, or updated at are in the params

--- app/controllers/api/v1/transactions/find_controller.rb
+++ app/controllers/api/v1/transactions/find_controller.rb
@@ -24,4 +24,27 @@ class Api::V1::Transactions::FindController < ApplicationController
       render json: TransactionSerializer.new(Transaction.find_by(created_at: created_at))
     end
   end
+
+  def index
+    if params.keys.include?("invoice_id")
+      render json: TransactionFindAllSerializer.new(Transaction.find_by_invoice_id(params["invoice_id"]))
+    elsif params.keys.include?("updated_at")
+      transactions = Transaction.find_by_updated_at(params["updated_at"])
+      render json: TransactionFindAllSerializer.new(transactions)
+    elsif params.keys.include?("created_at")
+      transactions = Transaction.find_by_created_at(params["created_at"])
+      render json: TransactionFindAllSerializer.new(transactions)
+    elsif params.keys.include?("credit_card_number")
+      transactions = Transaction.find_by_credit_card_number(params["credit_card_number"])
+      render json: TransactionFindAllSerializer.new(transactions)
+    elsif params.keys.include?("credit_card_expiration_date")
+      transactions = Transaction.find_by_credit_card_expiration_date(params["credit_card_expiration_date"])
+      render json: TransactionFindAllSerializer.new(transactions)
+    elsif params.keys.include?("result")
+      transactions = Transaction.find_by_result(params["result"])
+      render json: TransactionFindAllSerializer.new(transactions)
+    else params.keys.include?("id")
+      render json: TransactionFindAllSerializer.new(Transaction.find_by_id(params["id"]))
+    end
+  end
 end
