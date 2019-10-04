05ce002021f80d9ce307c6e3d34c19fd36b4f894
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 22:41:04 -0600

    Delete duplicate method find_by_created_at in invoice model

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,4 +7,32 @@ class InvoiceItem < ApplicationRecord
                         :unit_price,
                         :created_at,
                         :updated_at
+
+  def self.find_by_id(id)
+    where(id: id)
+  end
+
+  def self.find_by_created_at(created_at)
+    where(created_at: created_at)
+  end
+
+  def self.find_by_updated_at(updated_at)
+    where(updated_at: updated_at)
+  end
+
+  def self.find_by_invoice_id(invoice_id)
+    where(invoice_id: invoice_id)
+  end
+
+  def self.find_by_item_id(item_id)
+    where(item_id: item_id)
+  end
+
+  def self.find_by_unit_price(unit_price)
+    where(unit_price: unit_price)
+  end
+
+  def self.find_by_quantity(quantity)
+    where(quantity: quantity)
+  end
 end
