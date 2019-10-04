b939ade82c1717385b4734f2e5d2850f8bd240a2
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 22:19:12 -0600

    Add tests to invoice request spec that verfifies that we can find invoices based on their customer_id, merchant_id, created_at, updated_at, and status attributes

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -197,4 +197,129 @@ describe "Invoices API" do
     expect(invoices["data"].count).to eq(1)
     expect(invoices["data"].all? { |hash| hash["attributes"]["id"] == 1 }).to eq(true)
   end
+
+  it "finds all item record matches based on status" do
+    customer_1 = create(:customer, id: 1)
+    merchant_1 = create(:merchant, id: 1)
+    merchant_2 = create(:merchant, id: 2)
+    merchant_3 = create(:merchant, id: 3)
+    merchant_4 = create(:merchant, id: 4)
+    merchant_5 = create(:merchant, id: 5)
+    merchant_6 = create(:merchant, id: 6)
+    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id, id: 1, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id, id: 2, status: "shipped", updated_at: "2012-03-27 14:53:58 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id, id: 3, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_4 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 4, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_5 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 5, status: "not shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-29 16:12:25 UTC")
+    invoice_6 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_5.id, id: 6, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+
+    get "/api/v1/invoices/find_all?status=shipped"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(5)
+    expect(invoices["data"].all? { |hash| hash["attributes"]["status"] == "shipped" }).to eq(true)
+  end
+
+  it "finds all item record matches based on customer_id" do
+    customer_1 = create(:customer, id: 1)
+    merchant_1 = create(:merchant, id: 1)
+    merchant_2 = create(:merchant, id: 2)
+    merchant_3 = create(:merchant, id: 3)
+    merchant_4 = create(:merchant, id: 4)
+    merchant_5 = create(:merchant, id: 5)
+    merchant_6 = create(:merchant, id: 6)
+    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id, id: 1, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id, id: 2, status: "shipped", updated_at: "2012-03-27 14:53:58 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id, id: 3, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_4 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 4, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_5 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 5, status: "not shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-29 16:12:25 UTC")
+    invoice_6 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_5.id, id: 6, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+
+    get "/api/v1/invoices/find_all?customer_id=1"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(6)
+    expect(invoices["data"].all? { |hash| hash["attributes"]["customer_id"] == 1 }).to eq(true)
+  end
+
+  it "finds all item record matches based on merchant_id" do
+    customer_1 = create(:customer, id: 1)
+    merchant_1 = create(:merchant, id: 1)
+    merchant_2 = create(:merchant, id: 2)
+    merchant_3 = create(:merchant, id: 3)
+    merchant_4 = create(:merchant, id: 4)
+    merchant_5 = create(:merchant, id: 5)
+    merchant_6 = create(:merchant, id: 6)
+    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id, id: 1, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id, id: 2, status: "shipped", updated_at: "2012-03-27 14:53:58 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id, id: 3, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_4 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 4, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_5 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 5, status: "not shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-29 16:12:25 UTC")
+    invoice_6 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_5.id, id: 6, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+
+    get "/api/v1/invoices/find_all?merchant_id=3"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(1)
+    expect(invoices["data"].all? { |hash| hash["attributes"]["merchant_id"] == 3 }).to eq(true)
+  end
+
+  it "finds all item record matches based on created_at" do
+    customer_1 = create(:customer, id: 1)
+    merchant_1 = create(:merchant, id: 1)
+    merchant_2 = create(:merchant, id: 2)
+    merchant_3 = create(:merchant, id: 3)
+    merchant_4 = create(:merchant, id: 4)
+    merchant_5 = create(:merchant, id: 5)
+    merchant_6 = create(:merchant, id: 6)
+    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id, id: 1, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id, id: 2, status: "shipped", updated_at: "2012-03-27 14:53:58 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id, id: 3, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_4 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 4, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_5 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 5, status: "not shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-29 16:12:25 UTC")
+    invoice_6 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_5.id, id: 6, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+
+    get "/api/v1/invoices/find_all?created_at=2012-03-27 16:12:25 UTC"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(5)
+    expect(invoices["data"].all? { |hash| Time.parse(hash["attributes"]["created_at"]) == "2012-03-27 16:12:25 UTC" }).to eq(true)
+  end
+
+  it "finds all item record matches based on updated_at" do
+    customer_1 = create(:customer, id: 1)
+    merchant_1 = create(:merchant, id: 1)
+    merchant_2 = create(:merchant, id: 2)
+    merchant_3 = create(:merchant, id: 3)
+    merchant_4 = create(:merchant, id: 4)
+    merchant_5 = create(:merchant, id: 5)
+    merchant_6 = create(:merchant, id: 6)
+    invoice_1 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_1.id, id: 1, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_2 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_2.id, id: 2, status: "shipped", updated_at: "2012-03-27 14:53:58 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_3 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_3.id, id: 3, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_4 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 4, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+    invoice_5 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_4.id, id: 5, status: "not shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-29 16:12:25 UTC")
+    invoice_6 = create(:invoice, customer_id: customer_1.id, merchant_id: merchant_5.id, id: 6, status: "shipped", updated_at: "2012-03-27 14:53:59 UTC", created_at: "2012-03-27 16:12:25 UTC")
+
+    get "/api/v1/invoices/find_all?updated_at=2012-03-27 14:53:59 UTC"
+
+    expect(response).to be_successful
+
+    invoices = JSON.parse(response.body)
+
+    expect(invoices["data"].count).to eq(5)
+    expect(invoices["data"].all? { |hash| Time.parse(hash["attributes"]["updated_at"]) == "2012-03-27 14:53:59 UTC" }).to eq(true)
+  end
 end
