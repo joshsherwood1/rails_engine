ba76291c161359680305629e2273f8eb5636ffa0
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 23:38:41 -0600

    Add route for finding all customers based on shared attributes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
       end
       namespace :customers do
         get "/find", to: "find#show"
+        get "/find_all", to: "find#index"
       end
       namespace :transactions do
         get "/find", to: "find#show"
