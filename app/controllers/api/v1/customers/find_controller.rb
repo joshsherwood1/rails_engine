1d1a6e6b6e63e5d0a790624a88def5bccad9e5d1
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 23:39:42 -0600

    Create index action in customers find controller with conditionals to route requests based on if id, first_name, last_name, created_at, or updated_at are in the params

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -17,4 +17,21 @@ class Api::V1::Customers::FindController < ApplicationController
       render json: CustomerSerializer.new(Customer.find_by(created_at: created_at))
     end
   end
+
+  def index
+    if params.keys.include?("first_name")
+      render json: CustomerFindAllSerializer.new(Customer.find_by_first_name(params["first_name"]))
+    elsif params.keys.include?("updated_at")
+      customers = Customer.find_by_updated_at(params["updated_at"])
+      render json: CustomerFindAllSerializer.new(customers)
+    elsif params.keys.include?("last_name")
+      customers = Customer.find_by_last_name(params["last_name"])
+      render json: CustomerFindAllSerializer.new(customers)
+    elsif params.keys.include?("created_at")
+      customers = Customer.find_by_created_at(params["created_at"])
+      render json: CustomerFindAllSerializer.new(customers)
+    else params.keys.include?("id")
+      render json: CustomerFindAllSerializer.new(Customer.find_by_id(params["id"]))
+    end
+  end
 end
