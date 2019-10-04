4e6c00cd26f0cf5723007f4ef65f42d214d09334
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 18:23:18 -0600

    Create tests in items request spec that tests that we can find all items based on shared attributes id, name, description, created_at, updated_at, and unit price

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -11,7 +11,7 @@ describe "Items API" do
 
     items = JSON.parse(response.body)
 
-    expect(items["data"].count).to eq(3)
+    expect(items["data"].count).to eq(9)
   end
 
   it "can get one item by its id" do
@@ -174,5 +174,92 @@ describe "Items API" do
     expect(items["data"].count).to eq(3)
     #binding.pry
     expect(items["data"].all? { |hash| hash["attributes"]["merchant_id"] == 2 }).to eq(true)
+  end
+
+  before(:each) do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    @merchant_3 = create(:merchant)
+    @merchant_4 = create(:merchant)
+    @merchant_5 = create(:merchant)
+    @item_1 = create(:item, unit_price: 12345, merchant_id: @merchant_5.id, name: "Esso Dei", id: 123456789, description: "great!", updated_at: "2012-02-27 15:53:59 UTC", created_at: "2012-03-27 14:53:59 UTC")
+    @item_2 = create(:item, unit_price: 22345, merchant_id: @merchant_1.id, name: "Esso Deii", id: 223456789, description: "great!!", updated_at: "2012-02-27 14:53:59 UTC", created_at: "2012-03-27 14:53:59 UTC")
+    @item_3 = create(:item, unit_price: 32345, merchant_id: @merchant_2.id, name: "Esso Deia", id: 323456789, description: "greater!", updated_at: "2012-02-27 14:53:59 UTC", created_at: "2012-03-27 14:53:59 UTC")
+    @item_4 = create(:item, unit_price: 42345, merchant_id: @merchant_3.id, name: "Esso Deia", id: 423456789, description: "greatest!", updated_at: "2012-02-27 14:53:59 UTC", created_at: "2012-03-27 14:53:59 UTC")
+    @item_5 = create(:item, unit_price: 52345, merchant_id: @merchant_4.id, name: "Esso Deio", id: 523456789, description: "best!", updated_at: "2012-02-27 14:53:59 UTC", created_at: "2012-03-27 14:54:59 UTC")
+    @item_6 = create(:item, unit_price: 62345, merchant_id: @merchant_4.id, name: "Esso Deiu", id: 623456789, description: "good!", updated_at: "2012-02-27 14:53:59 UTC", created_at: "2012-03-27 14:53:59 UTC")
+  end
+
+  it "finds all item record matches based on name" do
+
+    get "/api/v1/items/find_all?name=Esso Deii"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items["data"].count).to eq(1)
+
+    expect(items["data"].all? { |hash| hash["attributes"]["name"] == "Esso Deii" }).to eq(true)
+  end
+
+  it "finds all item record matches based on description" do
+
+    get "/api/v1/items/find_all?description=best!"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items["data"].count).to eq(1)
+    expect(items["data"].all? { |hash| hash["attributes"]["description"] == "best!" }).to eq(true)
+  end
+
+  it "finds all item record matches based on id" do
+
+    get "/api/v1/items/find_all?id=423456789"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items["data"].count).to eq(1)
+    expect(items["data"].all? { |hash| hash["attributes"]["id"] == 423456789 }).to eq(true)
+  end
+
+  it "finds all item record matches based on unit_price" do
+
+    get "/api/v1/items/find_all?unit_price=32345"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items["data"].count).to eq(1)
+    expect(items["data"].all? { |hash| hash["attributes"]["unit_price"] == 32345 }).to eq(true)
+  end
+
+  it "finds all item record matches based on created_at" do
+
+    get "/api/v1/items/find_all?created_at=2012-03-27 14:53:59 UTC"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items["data"].count).to eq(5)
+    expect(items["data"].all? { |hash| Time.parse(hash["attributes"]["created_at"]) == "2012-03-27 14:53:59 UTC" }).to eq(true)
+  end
+
+  it "finds all item record matches based on updated_at" do
+
+    get "/api/v1/items/find_all?updated_at=2012-02-27 14:53:59 UTC"
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+
+    expect(items["data"].count).to eq(5)
+    expect(items["data"].all? { |hash| Time.parse(hash["attributes"]["updated_at"]) == "2012-02-27 14:53:59 UTC" }).to eq(true)
   end
 end
