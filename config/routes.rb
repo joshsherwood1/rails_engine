d873dfcef16a34fab3084530c08f4ad05e3feece
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-03 22:43:09 -0600

    Create route for finding all invoice items with shared attributes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
       end
       namespace :invoice_items do
         get "/find", to: "find#show"
+        get "/find_all", to: "find#index"
       end
       namespace :customers do
         get "/find", to: "find#show"
