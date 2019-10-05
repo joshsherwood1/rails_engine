dd67a88e38772e7862e9e7081f9577fc26425438
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-04 20:37:35 -0600

    Add route for finding invoices for a merchant

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         get "/find", to: "find#show"
         get "/find_all", to: "find#index"
         get ":id/items", to: "items#index"
+        get ":id/invoices", to: "invoices#index"
       end
       namespace :items do
         get "/find", to: "find#show"
