a7037d3d0c95618b2dd13923b99344aa8bfb9934
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-04 20:43:28 -0600

    Modify unit_price attribute in invoicefindallserializer and invoiceitemfindallserializer so that unit price is divided by 100 and given in a string

--- app/serializers/invoice_item_find_all_serializer.rb
+++ app/serializers/invoice_item_find_all_serializer.rb
@@ -1,4 +1,8 @@
 class InvoiceItemFindAllSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :item_id, :invoice_id, :quantity, :unit_price, :created_at, :updated_at
+
+  attribute :unit_price do |object|
+   (object.unit_price / 100).round(2).to_s
+  end
 end
