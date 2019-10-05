30b95c0160234d7da07df4c2c23d7602654bd926
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-04 22:36:38 -0600

    Add route for obtaining item assocated with item invoice

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
       namespace :invoice_items do
         get "/find", to: "find#show"
         get "/find_all", to: "find#index"
+        get "/:id/item", to: "item#index"
       end
       namespace :customers do
         get "/find", to: "find#show"
