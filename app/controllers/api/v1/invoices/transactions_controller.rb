54583c1e7895b8c7d0e34ab7a53f6a3f0dcfca45
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-04 21:12:29 -0600

    Change names of instance variables in index action in invoices transactions controller

--- app/controllers/api/v1/invoices/transactions_controller.rb
+++ app/controllers/api/v1/invoices/transactions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Invoices::TransactionsController < ApplicationController
 
   def index
-    @transactions_for_merchant = Invoice.find(params[:id]).transactions
-    render json: TransactionSerializer.new(@transactions_for_merchant)
+    @transactions_for_invoice = Invoice.find(params[:id]).transactions
+    render json: TransactionSerializer.new(@transactions_for_invoice)
   end
 end
