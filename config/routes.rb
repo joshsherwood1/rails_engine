38828bbc49c9d41efbffbc47f33e3467ab60f02b
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-04 23:11:43 -0600

    Add route for obtaining invoices associated with customer

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
       namespace :customers do
         get "/find", to: "find#show"
         get "/find_all", to: "find#index"
+        get "/:id/invoices", to: "invoices#index"
       end
       namespace :transactions do
         get "/find", to: "find#show"
