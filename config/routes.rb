5f5ec1105147f932c641b58d94ed0f160a4ba2c6
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-06 00:31:08 -0600

    Add routes for returning total revenue on a specified date for all merchants, the customer who had the most successful transactions for a merchant, the top specified number of items that brought in the most revenue, and the the date with the most sales for the given item using the invoice date

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,15 @@ Rails.application.routes.draw do
         get "/:id/items", to: "items#index"
         get "/:id/invoices", to: "invoices#index"
         get "/random", to: "random#show"
+        get "/revenue", to: "revenue#index"
+        get "/:id/favorite_customer", to: "favorite_customer#show"
       end
       namespace :items do
         get "/find", to: "find#show"
         get "/find_all", to: "find#index"
         get "/random", to: "random#show"
+        get "/most_revenue", to: "most_revenue#index"
+        get ":id/best_day", to: "best_day#show"
       end
       namespace :invoices do
         get "/find", to: "find#show"
