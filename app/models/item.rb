3d05435a90210e0a3716601354ffca66d8cdeeaf
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-06 00:32:29 -0600

    Add unfinished class and instance methods in item and merchant class for completng complex database inquiries

--- app/models/item.rb
+++ app/models/item.rb
@@ -40,4 +40,15 @@ class Item < ApplicationRecord
   def self.get_random_id
     pluck(:id).sample
   end
+
+  def self.items_with_most_revenue(quantity)
+    joins(:invoice_items).group(:id).select("items.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue").order("revenue desc").limit(quantity)
+    # should the following below work????
+    #joins(:invoice_items).joins(:transactions).group(:id).merge(Transaction.successful).select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue").order("revenue desc").limit(quantity)
+  end
+
+  def find_best_day
+    binding.pry
+    invoices.joins(:invoice_items, :transactions).group("invoices.created_at").merge(Transaction.successful).select("invoices.created_at, sum(invoice_items.quantity * invoice_items.unit_price) as revenue").order("revenue desc").limit(1)
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -14,7 +14,7 @@ class Merchant < ApplicationRecord
   end
 
   def self.find_by_name(name)
-    where(name: name)
+    where("LOWER(name) = ?", name.downcase)
   end
 
   def self.find_by_id(id)
@@ -31,5 +31,18 @@ class Merchant < ApplicationRecord
 
   def self.get_random_id
     pluck(:id).sample
+  end
+
+  def self.total_revenue_for_specified_day(date)
+    today = Date.strptime(date, '%Y-%m-%d')
+    tomorrow = today + 1
+    tomorrow_sql = tomorrow.strftime('%Y-%m-%d')
+    binding.pry
+    joins(:transactions, :invoice_items).merge(Transaction.successful).where("transactions.created_at > ?", today).where("transactions.created_at < ?", tomorrow_sql).sum("invoice_items.quantity * invoice_items.unit_price")
+  end
+
+  def find_favorite_customer
+    #binding.pry
+    invoices.joins(:transactions, :invoice_items, :customers).group(:customer_id).merge(Transaction.successful).select("customers.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue").order("revenue desc").limit(1)
   end
 end
